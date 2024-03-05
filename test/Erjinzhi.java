484d1dbc5806f00a7624f243ecd6b810fa493aad
彬彬 马 <188821808@qq.com>
2024-03-05 21:06:02 +0800

    try

---
+++ test/Erjinzhi.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Erjinzhi{
+	public static void main(String args[]){
+		Scanner s=new Scanner(System.in);
+		int num=s.nextInt();
+		System.out.println(Integer.toBinaryString(num));
+		
+	}
+}
